441619192cbaa51d421e9cda47a73c664c7fece8
Michał Grzegorczyk <micgrzegorczyk+github@gmail.com>
2021-01-22 22:18:43 +0100

    Added react-query and react-router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
 import './App.css';
+import { useQuery } from 'react-query';
+import { Route, Link, useParams } from 'react-router-dom';
+
+function Pokemon() {
+  const { name } = useParams<{ name: string }>();
+  return (
+    <h2>
+      <Link to="/">back</Link>
+      {name}
+    </h2>
+  );
+}
 
 function App() {
+  const { error, isLoading, data } = useQuery<{ name: string }[]>(
+    'pokemon',
+    async () => {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+      const json = await response.json();
+      return json.results;
+    },
+  );
+
+  if (isLoading) return <h1>'Loading...'</h1>;
+
+  if (error) return <p>An error has occurred: {String(error)}</p>;
+
+  console.log(data);
+
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <ul className="App">
+        {data!.map(({ name }) => (
+          <li>
+            <Link to={`/${name}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+      <Route path="/:name" component={Pokemon} />
+    </>
   );
 }
 
