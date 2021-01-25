0d91510a0affef156d127b16e8031f3e35712ed7
Michał Grzegorczyk <micgrzegorczyk+github@gmail.com>
2021-01-25 21:46:51 +0100

    Added axios and used it inside function

--- app/public/functions/auth.js
+++ app/public/functions/auth.js
@@ -1,6 +1,32 @@
+import axios from 'axios';
+
 exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
-  };
+  const code = event.queryStringParameters.code || '';
+  const state = event.queryStringParameters.state || '';
+
+  if (!code || !state) {
+    return {
+      statusCode: 302,
+      headers: {
+        Location: 'hhttps://collibra.softsoft.dev',
+      },
+    };
+  }
+
+  try {
+    const response = await axios.post(process.env.DRIBBLE_URL, {
+      client_id: process.env.DRIBBLE_CLIENT_ID,
+      client_secret: process.env.DRIBBLE_SECRET_ID,
+      code: code,
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Hello World' }),
+    };
+  }
 };
